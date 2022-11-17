fbaa761e1a678df574610405191b56eb7638056f
尒貴ふ <96385808+ExPensive-58@users.noreply.github.com>
2022-11-17 17:09:56 +0800

    Create caiyun.js

---
+++ caiyun.js
@@ -0,0 +1,3 @@
+# > 031 彩云天气 （2020-11-21） by ddgksf2013
+https?:\/\/biz\.caiyunapp\.com\/(membership_rights|v2\/user) url script-response-body https://codeberg.org/ddgksf2013/Cuttlefish/raw/branch/master/Crack/caiyun.js
+hostname = biz.caiyunapp.com
